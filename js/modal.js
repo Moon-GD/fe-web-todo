0f9f89090f4baf6d1c06f1bf3f5f075bc74f22e6
RentalHub-MAC23 <king4mun@gmail.com>
2023-01-05 16:50:49 +0900

    add: way to delete cards in modal

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+import { clickedCard } from "./section.js"
+
+const modalCloseBtn = document.getElementById("modal-cancel-button");
+const modalDeleteBtn = document.getElementById("modal-delete-button");
+const modalFrame = document.querySelector("section#modal-area")
+
+modalCloseBtn.addEventListener("click", () => {
+    modalFrame.style.display = "none";
+})
+
+modalDeleteBtn.addEventListener("click", () => {
+    modalFrame.style.display = "none";
+    clickedCard.remove()
+})
--- js/section.js
+++ js/section.js
@@ -7,19 +7,31 @@ const CARD_BORDER_HOVER = "1px solid " + CARD_BTN_HOVER;
 const CARD_BACKGROUND_HOVER = "#FFEEEC";
 
 let cardCloseBtns = document.querySelectorAll(".card-close-button");
+let clickedCard = ""
 
 cardCloseBtns.forEach((btn) => {
     let cardFrame = btn.parentElement.parentElement;
+    const modalFrame = document.querySelector("section#modal-area")
 
+    // click -> 모달 켜기
+    btn.addEventListener("click", () => {
+        modalFrame.style.display = "flex";
+        clickedCard = btn.parentElement.parentElement
+    })
+
+    // hover -> 카드 css 적용
     btn.addEventListener("mouseover", () => {
         btn.style.color = CARD_BTN_HOVER;
         cardFrame.style.border = CARD_BORDER_HOVER;
         cardFrame.style.backgroundColor = CARD_BACKGROUND_HOVER;
     })
 
+    // hover out -> 카드 css 초기화
     btn.addEventListener("mouseout", () => {
         btn.style.color = CARD_BTN_ORIGINAL;
         cardFrame.style.border = CARD_BORDER_ORIGINAL;
         cardFrame.style.backgroundColor = CARD_BACKGROUND_ORIGINAL;
     })
 })
+
+export { clickedCard }
