a7db5a71d394b02b171e383a427f37457a16c833
miinjoo <minjoo9903@ewhain.net>
2023-01-06 12:33:48 +0900

    feat:카드 추가

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 //menu 열기
-const open = () => {
-  document.querySelector(".menuWrapper").classList.add("sidebarshow");
+const itemAddBtn = document.getElementById("addBtn");
+
+const visibleTodo = () => {
+  document.querySelector(".itemTodoAddBox").classList.remove("hidden");
 };
-//menu닫기
-const close = () => {
-  document.querySelector(".menuWrapper").classList.remove("sidebarshow");
+const unvisibleTodo = () => {
+  document.querySelector(".itemTodoAddBox").classList.add("hidden");
 };
-document.getElementById("menuCloseBtn").addEventListener("click", close);
-document.getElementById("hamburgerBtn").addEventListener("click", open);
+
+const visibleDoing = () => {
+  document.querySelector(".itemDoingAddBox").classList.remove("hidden");
+};
+const unvisibleDoing = () => {
+  document.querySelector(".itemDoingAddBox").classList.add("hidden");
+};
+
+const visibleDone = () => {
+  document.querySelector(".itemDoneAddBox").classList.remove("hidden");
+};
+const unvisibleDone = () => {
+  document.querySelector(".itemDoneAddBox").classList.add("hidden");
+};
+
+document.getElementById("addTodoBtn").addEventListener("click", visibleTodo);
+document.getElementById("addDoingBtn").addEventListener("click", visibleDoing);
+document.getElementById("addDoneBtn").addEventListener("click", visibleDone);
+
+document
+  .querySelector(".cancelTodoBtn")
+  .addEventListener("click", unvisibleTodo);
+document
+  .querySelector(".cancelDoingBtn")
+  .addEventListener("click", unvisibleDoing);
+document
+  .querySelector(".cancelDoneBtn")
+  .addEventListener("click", unvisibleDone);
